9c67b384f20760e17c1d3049eee2ee2dd009bd78
DESKTOP-F8120DF\Minh_Son <kakashi01vl@gmail.com>
2025-05-05 23:26:10 +0900

    logic tieeu hao charka

--- bot/services/battle.py
+++ bot/services/battle.py
@@ -47,7 +47,7 @@ class Battle:
         if atk.chakra >= 100 and not is_rooted:
             logs.append(f"{atk.name} dùng kỹ năng đặc biệt!")
             logs += atk.special_skills()
-            atk.chakra = 0
+            atk.chakra -= 100
         else:
             if atk.chakra >= 100 and is_rooted:
                 logs.append(f"🚫 {atk.name} đang bị trói chân, không thể dùng kỹ năng!")
