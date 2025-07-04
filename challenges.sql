0bcb6989796acec6baa1c952ba67cb982c8b6c75
STONE-SON <son.d.m@stonesystem.co.jp>
2025-07-04 09:13:43 +0900

    không cần thiết

--- challenges.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE challenges (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    card_name VARCHAR(100) NOT NULL,
-    card_strength INT NOT NULL,
-    image_url_key VARCHAR(255) NOT NULL,
-    narrative TEXT,
-    bonus_ryo INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
